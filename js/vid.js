d717631d0bef8e34b1484c2297378c56a3adaf32
Noah Smith <bigbass98@gmail.com>
2018-10-16 18:40:35 -0500

    Edits
    Minor Edits

--- js/vid.js
+++ js/vid.js
@@ -1,7 +1,4 @@
 // JavaScript Document
 $('video').mediaelementplayer({
-	features: ['playpause', 'current', 'progress', 'duration', 'volume', 'fullscreen'],
-	startLanguage: 'en', 
-	stretching: 'responsive',
-	enableAutoSize: true
+	features: ['playpause', 'current', 'progress', 'duration', 'volume', 'fullscreen']
 });
